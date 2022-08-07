dce2adab92d444d997c59245b532cd1f1f1d6718
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-08-07 11:04:48 +0200

    Started working on the post request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,15 +13,26 @@ function App(){
         console.log(usersList);
     }, [])
 
+    
+
+    
     return (
-        <div>
+        <div style={{textAlign: 'center'}}>
             <h1>Users : </h1>
-            {usersList.map(user => (
-                <div>
-                    <h1>{user.name}</h1>
-                    <h1>{user.username}</h1>
-                </div>
-            ))}
+            <div style={{display: 'flex', gap: '10px', flexDirection: "column"}}>
+                {usersList.map(user => (
+                    <div className="">
+                        <h1>User with id of : {user._id} </h1>
+                        <h1>Name: {user.name}</h1>
+                        <h1>Age : {user.age}</h1>
+                        <h1>Username : {user.username}</h1>
+                    </div>
+                ))}
+            </div>
+            <input type="text" placeholder="Enter your name" />
+            <input type="number" placeholder="Enter your age" />
+            <input type="text" placeholder="Enter your username" />
+            <button>Create User</button>
         </div>
     )
 }
